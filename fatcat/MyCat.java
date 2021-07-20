57ca9b6291943a49d1f01a56d7ec4c2ab113c46a
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2021-07-20 07:58:08 -0600

    AgregandoFatCat
    Se subió todo lo de fatcat

---
+++ fatcat/MyCat.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * MyCat is your own cat. Get it to do things by writing code in its act method.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyCat extends Cat
+{
+    /**
+     * Act - do whatever the MyCat wants to do.
+     */
+    public void act()
+    {
+        if (isSleepy()){
+            sleep(1);
+            shoutHooray();
+            
+        } else {
+            shoutHooray();
+            dance();
+
+        }
+        if (isBored()){
+            dance();
+        }
+        if (isHungry()){
+            eat();
+        }
+        if (isAlone()){
+            sleep(1);
+            
+        } else {
+            shoutHooray();
+        }
+    }    
+}
